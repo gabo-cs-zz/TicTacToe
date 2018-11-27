c5f76286bed7979cc6e29d4f2395200a90355e94
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-11-26 20:46:02 -0500

    More JS Displaying values, player..

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,5 +1,5 @@
 // Coding the JS
-var sw;
+var sw; // sw = O, !sw = X.
 var plyr = document.getElementById("playerNumber");
 var btnO = document.getElementById("O");
 var btnX = document.getElementById("X");
@@ -8,9 +8,31 @@ const ROWS = tb.rows;
 btnX.addEventListener("click", ()=> actionate(false, "X"));
 btnO.addEventListener("click", ()=> actionate(true, "O"));
 
+for(let i of ROWS){
+  for(let j of i.cells){
+    j.addEventListener("click", ()=> {
+      if (sw) {
+        changeValues(j, "O", "X");
+      } else {
+        changeValues(j, "X", "O");
+      }
+      sw = !sw;
+    });
+  }
+}
+
 function actionate(val, letter){
   btnO.disabled = true;
   btnO.classList.remove("fx");
   btnX.disabled = true;
   btnX.classList.remove("fx");
+  sw = val;
+  plyr.innerHTML = letter;
+  tb.style.pointerEvents = "auto";
+}
+
+function changeValues(elem, str1, str2){
+  elem.innerHTML = str1;
+  elem.style.pointerEvents = "none";
+  plyr.innerHTML = str2;
 }
