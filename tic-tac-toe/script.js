1c42669b7877d407c1d3e9965185c4add4d145a2
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-11-26 21:12:05 -0500

    Implementing first winning Horizontal and Vertical winning implemented

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -22,10 +22,17 @@ for(let i of ROWS){
         rows_x.push(i.rowIndex);
         cols_x.push(j.cellIndex);
       }
+      if ( winSameLine(rows_o, cols_o) || winSameLine(cols_o, rows_o) ){
+        win("O");
+      } else if( winSameLine(rows_x, cols_x) || winSameLine(cols_x, rows_x) ){
+        win("X");
+      } else {
+        if (cols_o.length == 5 || cols_x.length == 5) win('T');
+      }
       sw = !sw;
-      console.log(j.innerHTML +' '+i.rowIndex +' '+j.cellIndex );
+      /*console.log(j.innerHTML +' '+i.rowIndex +' '+j.cellIndex );
       console.log(rows_o);
-      console.log(cols_o);
+      console.log(cols_o);*/
     });
   }
 }
@@ -44,4 +51,36 @@ function changeValues(elem, str1, str2){
   elem.innerHTML = str1;
   elem.style.pointerEvents = "none";
   plyr.innerHTML = str2;
+}
+
+function win(letter){
+  if(letter != "X" && letter != "O"){
+    setTimeout( ()=> alert("IT'S A TIE."), 150 );
+  } else {
+    setTimeout( ()=> alert(`${letter} WINS.`), 150 );
+    tb.style.pointerEvents = "none";
+  }
+}
+
+function winSameLine(arr, arr2){
+  var cnt = 0, cnt2 = 0, cnt3 = 0;
+  var sum = 0, sum2 = 0, sum3 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case 0:
+        sum += arr2[i];
+        cnt++;
+        break;
+      case 1:
+        sum2 += arr2[i];
+        cnt2++;
+        break;
+      case 2:
+        sum3 += arr2[i];
+        cnt3++;
+        break;
+    }
+  }
+  return ( (sum == 3 && cnt == 3) || (sum2 == 3 && cnt2 == 3)
+    || (sum3 == 3 && cnt3 == 3)  );
 }
